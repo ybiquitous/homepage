5e695fb2b026534d3a121bd44ad28776453da45b
Masafumi Koba <ybiquitous@gmail.com>
2019-03-26 23:40:44 +0900

    Apply mdx-deck v2

--- src/slides/theme.js
+++ src/slides/theme.js
@@ -1,10 +1,6 @@
 /* eslint-disable import/no-internal-modules, import/no-extraneous-dependencies */
-import okaidia from "react-syntax-highlighter/styles/prism/okaidia";
-import theme from "mdx-deck/themes";
+import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
+import theme, { syntaxHighlighterPrism } from "@mdx-deck/themes";
 /* eslint-enable import/no-internal-modules, import/no-extraneous-dependencies */
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  ...theme,
-  prism: { style: okaidia },
-};
+export default syntaxHighlighterPrism({ ...theme, prism: { style: okaidia } });

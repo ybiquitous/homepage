aff408f7a0716f46d82c893c89b330e0e2d8a782
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2019-07-03 03:29:09 +0900

    [Blog] SPA redirect settings on Netlify (#95)

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -2,7 +2,8 @@ import metadata from "~blog/metadata.yml";
 import startBlog from "./2019/start-blog.md";
 import typeCompatibilityInTypeScript from "./2019/type-compatibility-in-typescript.md";
 import webpackOnRails from "./2019/webpack-on-rails.md";
+import spaRedirectOnNetlify from "./2019/spa-redirect-settings-on-netlify.md";
 
-const contents = [startBlog, typeCompatibilityInTypeScript, webpackOnRails];
+const contents = [startBlog, typeCompatibilityInTypeScript, webpackOnRails, spaRedirectOnNetlify];
 
 export const blogs = metadata.map((meta, index) => ({ ...meta, content: contents[index] }));

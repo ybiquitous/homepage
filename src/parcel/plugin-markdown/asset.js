2edd0d5b07c27cac8419089e306b2ca3abdd1e57
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-05-13 01:12:00 +0900

    Turn off `@typescript-eslint/no-var-requires`

--- src/parcel/plugin-markdown/asset.js
+++ src/parcel/plugin-markdown/asset.js
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { Asset } = require("parcel-bundler");
 const marked = require("marked");
 const hljs = require("highlight.js");
-/* eslint-enable @typescript-eslint/no-var-requires */
 
 const markedOptions = {
   breaks: true,

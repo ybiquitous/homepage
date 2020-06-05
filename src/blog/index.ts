9cffaa57f7f4dd69f4b62cfd0c3e835a2d10b305
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-06-06 03:07:59 +0900

    Fix build error (#324)
    > src/blog/index.ts(3,8): error TS2300: Duplicate identifier 'blog_2019_03'.
    > src/blog/index.ts(4,8): error TS2300: Duplicate identifier 'blog_2019_03'.

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -1,8 +1,8 @@
 import blog_2019_01 from "./2019/start-blog.md";
 import blog_2019_02 from "./2019/type-compatibility-in-typescript.md";
 import blog_2019_03 from "./2019/webpack-on-rails.md";
-import blog_2019_03 from "./2019/spa-redirect-settings-on-netlify.md";
-import blog_2019_04 from "./2019/create-pull-request-on-command-line.md";
+import blog_2019_04 from "./2019/spa-redirect-settings-on-netlify.md";
+import blog_2019_05 from "./2019/create-pull-request-on-command-line.md";
 import blog_2020_01 from "./2020/using-nvm-on-github-actions.md";
 import metadata from "~blog/metadata.yml";
 
@@ -10,8 +10,8 @@ const contents = [
   blog_2019_01,
   blog_2019_02,
   blog_2019_03,
-  blog_2019_03,
   blog_2019_04,
+  blog_2019_05,
   blog_2020_01,
 ];
 

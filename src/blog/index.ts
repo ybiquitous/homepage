29bc83df9152067e25df8209e18ec778cc333a17
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-06-06 02:58:12 +0900

    Blog: Using nvm on GitHub Actions (#323)

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -1,16 +1,18 @@
-import startBlog from "./2019/start-blog.md";
-import typeCompatibilityInTypeScript from "./2019/type-compatibility-in-typescript.md";
-import webpackOnRails from "./2019/webpack-on-rails.md";
-import spaRedirectOnNetlify from "./2019/spa-redirect-settings-on-netlify.md";
-import createPullRequestOnCommandLine from "./2019/create-pull-request-on-command-line.md";
+import blog_2019_01 from "./2019/start-blog.md";
+import blog_2019_02 from "./2019/type-compatibility-in-typescript.md";
+import blog_2019_03 from "./2019/webpack-on-rails.md";
+import blog_2019_03 from "./2019/spa-redirect-settings-on-netlify.md";
+import blog_2019_04 from "./2019/create-pull-request-on-command-line.md";
+import blog_2020_01 from "./2020/using-nvm-on-github-actions.md";
 import metadata from "~blog/metadata.yml";
 
 const contents = [
-  startBlog,
-  typeCompatibilityInTypeScript,
-  webpackOnRails,
-  spaRedirectOnNetlify,
-  createPullRequestOnCommandLine,
+  blog_2019_01,
+  blog_2019_02,
+  blog_2019_03,
+  blog_2019_03,
+  blog_2019_04,
+  blog_2020_01,
 ];
 
 export const blogs = metadata.map((meta, index) => ({ ...meta, content: contents[index] }));

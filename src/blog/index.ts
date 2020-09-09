e6609c9d2641c6b38793351b482bb926c371d124
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-09-10 00:39:04 +0900

    [Blog] RubyKaigi Takeout 2020 (2) (#374)

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -5,7 +5,8 @@ import blog_2019_03 from "./2019/webpack-on-rails.md";
 import blog_2019_04 from "./2019/spa-redirect-settings-on-netlify.md";
 import blog_2019_05 from "./2019/create-pull-request-on-command-line.md";
 import blog_2020_01 from "./2020/using-nvm-on-github-actions.md";
-import blog_2020_09 from "./2020/rubykaigi-takeout-2020.md";
+import blog_2020_02 from "./2020/rubykaigi-takeout-2020.md";
+import blog_2020_03 from "./2020/rubykaigi-takeout-2020-2.md";
 import metadata from "~blog/metadata.yml";
 
 const contents = [
@@ -15,7 +16,8 @@ const contents = [
   blog_2019_04,
   blog_2019_05,
   blog_2020_01,
-  blog_2020_09,
+  blog_2020_02,
+  blog_2020_03,
 ];
 /* eslint-enable camelcase */
 

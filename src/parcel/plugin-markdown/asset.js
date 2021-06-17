4f2fbf80af3f37e965772a286f76a19f86508f87
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-17 23:54:52 +0900

    Fix eslint errors

--- src/parcel/plugin-markdown/asset.js
+++ src/parcel/plugin-markdown/asset.js
@@ -1,8 +1,11 @@
+// @ts-expect-error
 const { highlight } = require("highlight.js");
 const marked = require("marked");
+// @ts-expect-error
 const { Asset } = require("parcel-bundler");
 
 class MarkdownAsset extends Asset {
+  // @ts-expect-error
   constructor(name, options) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super(name, options);
@@ -12,14 +15,19 @@ class MarkdownAsset extends Asset {
 
   generate() {
     class MyRenderer extends marked.Renderer {
+      /**
+       * @param {string | null} href
+       * @param {string | null} title
+       * @param {string} text
+       */
       link(href, title, text) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
         return super.link(href == null ? href : href.replace(/\.md$/u, ""), title, text);
       }
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    // @ts-expect-error
     return marked(this.contents, {
+      // eslint-disable-line @typescript-eslint/no-unsafe-argument
       breaks: true,
       highlight(code, lang) {
         return typeof lang === "string" && lang.length > 0 // eslint-disable-line @typescript-eslint/no-unsafe-return
--- src/parcel/plugin-markdown/index.js
+++ src/parcel/plugin-markdown/index.js
@@ -1,4 +1,6 @@
+/**
+ * @param {import("parcel-bundler")} bundler
+ */
 module.exports = (bundler) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   bundler.addAssetType(".md", require.resolve("./asset"));
 };

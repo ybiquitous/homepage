08047f94663275a331f1b1153b4c809147e67072
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-22 21:47:30 +0900

    Fix eslint issues

--- src/remark/remark-remove-markdown-extension.js
+++ src/remark/remark-remove-markdown-extension.js
@@ -1,11 +1,12 @@
-const visit = require("unist-util-visit");
+/* eslint-env node */
+const visit = require("unist-util-visit"); // eslint-disable-line import/no-extraneous-dependencies
 
 /** @type {import("unified").Plugin} */
 module.exports = function removeMarkdownExtension() {
   return (tree) => {
     visit(tree, "link", (node) => {
       if ("url" in node && typeof node.url === "string") {
-        node.url = node.url.replace(/\.md$/, "");
+        node.url = node.url.replace(/\.md$/u, "");
       }
     });
   };

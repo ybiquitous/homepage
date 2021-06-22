aace238ee0731f030ff0cae0b9a18f2e6f27b491
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-22 21:56:44 +0900

    refactor

--- src/remark/remark-relative-link.js
+++ src/remark/remark-relative-link.js
@@ -2,7 +2,7 @@
 const visit = require("unist-util-visit"); // eslint-disable-line import/no-extraneous-dependencies
 
 /** @type {import("unified").Plugin} */
-module.exports = function removeMarkdownExtension() {
+module.exports = () => {
   return (tree) => {
     visit(tree, "link", (node) => {
       if ("url" in node && typeof node.url === "string" && !node.url.startsWith("http")) {

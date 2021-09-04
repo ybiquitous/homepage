c885147530cb5024c903a94e3d0e9609ee626eef
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-09-05 00:35:33 +0900

    fixup! Fix ESLint issues

--- src/remark/remark-relative-link.js
+++ src/remark/remark-relative-link.js
@@ -3,10 +3,10 @@ const visit = require("unist-util-visit"); // eslint-disable-line import/no-extr
 
 /** @type {import("unified").Plugin} */
 module.exports = function remarkRelativeLink() {
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types -- False positive.
   return (tree) => {
-    // eslint-disable-line @typescript-eslint/prefer-readonly-parameter-types -- False positive.
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types -- False positive.
     visit(tree, "link", (node) => {
-      // eslint-disable-line @typescript-eslint/prefer-readonly-parameter-types -- False positive.
       if ("url" in node && typeof node.url === "string" && !node.url.startsWith("http")) {
         // eslint-disable-next-line no-param-reassign -- Ignorable.
         node.url = node.url.replace(/\.md$/u, "");

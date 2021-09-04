ee407050340cb59c62e38d625722ff1de6a630ff
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-09-04 12:13:34 +0900

    Replace `React.FC` type (#590)
    The type is discouraged. See below:
    - https://github.com/typescript-cheatsheets/react/blob/c7ca726c5d6452b6eb50fce435151463a6cf632a/README.md#function-components
    - https://github.com/facebook/create-react-app/pull/8177

--- src/pages/Home/Menu.jsx
+++ src/pages/Home/Menu.jsx
@@ -2,12 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "../../Link";
 
 /**
- * @type {React.FC<{
+ * @param {{
  *   link: string,
  *   icon: import("@fortawesome/react-fontawesome").FontAwesomeIconProps["icon"],
  *   content: string,
  *   subContent?: string,
- * }>}
+ * }} props
  */
 export const Menu = ({ link, icon, content, subContent }) => (
   <div className="text-xl border rounded shadow hover:shadow-lg hover:my-link-color hover:my-link-border-color transition-shadow">

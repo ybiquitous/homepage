e3b6ec4df13219527d5faaa3a9dfc53184d2cd9f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-09-23 22:48:58 +0900

    [Blog] RubyKaigi 2021 (#609)

--- src/blog/index.js
+++ src/blog/index.js
@@ -6,6 +6,13 @@ import metadata from "./metadata.json";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
 const content = async (slug) => import(`./${slug}.md`).then((module) => module.default);
 
+/**
+ * @param {{ slug: string, title: string, published: string | null }} navi
+ */
+const buildNavi = ({ slug, title, published }) => {
+  return published != null ? { path: `/blog/${slug}`, title } : null;
+};
+
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types -- False positive.
 export const blogs = metadata.map((meta, index, array) => {
   const prev = array[index - 1];
@@ -14,7 +21,7 @@ export const blogs = metadata.map((meta, index, array) => {
     ...meta,
     content,
     path: `/blog/${meta.slug}`,
-    prev: prev != null ? { path: `/blog/${prev.slug}`, title: prev.title } : null, // eslint-disable-line @typescript-eslint/no-unnecessary-condition
-    next: next != null ? { path: `/blog/${next.slug}`, title: next.title } : null, // eslint-disable-line @typescript-eslint/no-unnecessary-condition
+    prev: prev != null ? buildNavi(prev) : null, // eslint-disable-line @typescript-eslint/no-unnecessary-condition
+    next: next != null ? buildNavi(next) : null, // eslint-disable-line @typescript-eslint/no-unnecessary-condition
   };
 });

a49919111975e45079e5cf85c21050287302cf4b
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-11-05 00:34:44 +0900

    Adjust blog time styles

--- src/pages/BlogPost/Times.jsx
+++ src/pages/BlogPost/Times.jsx
@@ -7,20 +7,16 @@ export const Times = ({ published, lastUpdated }) => (
   <div className="text-sm my-text-gray flex flex-col sm:flex-row sm:gap-12">
     {published != null ? (
       <span>
-        <em>
-          <Time date={new Date(published)} />
-        </em>
-        <span className="ml-2">published</span>
+        <Time date={new Date(published)} />
+        <span className="ml-1">published</span>
       </span>
     ) : (
       <em>Unpublished</em>
     )}
     {lastUpdated != null && (
       <span>
-        <em>
-          <Time date={new Date(lastUpdated)} />
-        </em>
-        <span className="ml-2">updated</span>
+        <Time date={new Date(lastUpdated)} />
+        <span className="ml-1">updated</span>
       </span>
     )}
   </div>

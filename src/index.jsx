2c266ca814188b8a880e8dc2c846963c83c5a58c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-01-24 21:47:05 +0900

    Add Creative Commons License

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,22 @@ import "./index.css";
 import { render } from "react-dom";
 import { Router } from "./Router";
 
+const LicenseLink = () => (
+  <a className="my-link-color" href="https://creativecommons.org/licenses/by-sa/4.0/">
+    CC BY-SA 4.0
+  </a>
+);
+
 const Root = () => (
   <>
     <Router />
 
     <footer className="my-text-gray text-center mt-16">
       <p>
-        <small>© 2019-{new Date().getFullYear()} Masafumi Koba</small>
+        <small>
+          © 2019-{new Date().getFullYear()} Masafumi Koba. The content on this website is licensed
+          under a <LicenseLink /> license.
+        </small>
       </p>
     </footer>
   </>

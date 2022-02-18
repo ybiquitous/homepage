4961d75fe0286fd267b14fa0e4d45c409487f23e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-02-19 02:30:55 +0900

    Fix ESLint problems

--- src/pages/Home/Profile.jsx
+++ src/pages/Home/Profile.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Profile = () => (
   <div className="flex items-center">
--- src/utils/CopyToClipboard.jsx
+++ src/utils/CopyToClipboard.jsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 const INITIAL = "initial";

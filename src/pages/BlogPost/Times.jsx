0d5111fbfa0ff03500edd8ba5698f1a3eb76a3aa
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-02-19 11:47:15 +0900

    Simplify blog post time design
    - Trim `published` text
    - Refactor Flexbox properties

--- src/pages/BlogPost/Times.jsx
+++ src/pages/BlogPost/Times.jsx
@@ -4,19 +4,13 @@ import { Time } from "../../utils";
  * @param {{ published: string | null, lastUpdated: string | null }} props
  */
 export const Times = ({ published, lastUpdated }) => (
-  <div className="text-sm my-text-gray flex flex-col sm:flex-row sm:gap-12">
-    {published != null ? (
-      <span>
-        <Time date={new Date(published)} />
-        <span className="ml-1">published</span>
-      </span>
-    ) : (
-      <em>Unpublished</em>
-    )}
+  <div className="text-sm my-text-gray flex flex-wrap gap-x-4">
+    {published != null ? <Time date={new Date(published)} /> : <em>Draft</em>}
     {lastUpdated != null && (
       <span>
+        {"("}
         <Time date={new Date(lastUpdated)} />
-        <span className="ml-1">updated</span>
+        {" updated)"}
       </span>
     )}
   </div>

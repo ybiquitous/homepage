9cc42d20f6b75822980fb01a61ecdc4ab37b6dea
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-03-23 09:29:11 +0900

    Bump `@fortawesome/*` packages from 6.0.0 to 6.1.1
    This also changes a part of the blog post `/blog/2022/use-babel-macro-for-fontawesome-6`.
    
    See:
    - https://github.com/FortAwesome/Font-Awesome/releases/tag/6.1.1
    - https://github.com/FortAwesome/Font-Awesome/compare/6.0.0...6.1.1
    - https://fontawesome.com/docs/changelog/

--- src/types/fontawesome.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@fortawesome/fontawesome-svg-core/import.macro" {
-  import type { IconName, IconProp } from "@fortawesome/fontawesome-svg-core";
-
-  export function brands(iconName: IconName): IconProp;
-  export function solid(iconName: IconName): IconProp;
-  export function reqular(iconName: IconName): IconProp;
-}

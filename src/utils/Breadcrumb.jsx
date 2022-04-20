57e09e479df74be6f1e4e0fce9ded5bb69e951fa
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-04-21 02:20:59 +0900

    Fix broken `<Breadcrumb/>` className

--- src/utils/Breadcrumb.jsx
+++ src/utils/Breadcrumb.jsx
@@ -15,13 +15,14 @@ export const Breadcrumb = ({ items }) => (
     <ol className="flex flex-wrap">
       {[home, ...items].map((item, index, list) => {
         const current = index === list.length - 1;
-        const classNames = ["inline-flex items-center"];
+        const classNames = ["inline-flex", "items-center"];
         if (!current) {
-          classNames.push("after:content-['/'] after:text-xs after:mx-2");
+          classNames.push("after:content-['/']", "after:text-xs", "after:mx-2");
         }
+        const className = classNames.join(" ");
         const { el, key } = typeof item === "string" ? { el: item, key: item } : item;
         return (
-          <li key={key} aria-current={current ? "page" : undefined} className={classNames.join("")}>
+          <li key={key} aria-current={current ? "page" : undefined} className={className}>
             {isValidElement(el) ? cloneElement(el, { className: "my-text-secondary" }) : el}
           </li>
         );

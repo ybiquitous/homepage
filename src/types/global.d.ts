0684ff3acf8e8546cf1de3c37386f2182d85a26e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-04-23 10:27:22 +0900

    Fix ESLint errors

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,14 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface Window {
   /**
    * @see https://developer.twitter.com/en/docs/twitter-for-websites/embedded-tweets/guides/embedded-tweet-javascript-factory-function
    */
   twttr?: {
     widgets?: {
-      createTweet<E extends Element>(
+      createTweet: <E extends Element>(
         tweetID: string,
         targetEl: E,
-        options?: Record<string, unknown>
-      ): Promise<E>;
+        options?: ReadonlyRecord<string, unknown>
+      ) => Promise<E>;
     };
   };
 }

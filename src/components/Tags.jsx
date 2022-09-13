08fb416d45df1a90fada587d2682ff33de274fc7
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-13 13:55:45 +0900

    Refactor code (#961)

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -5,8 +5,8 @@ import { Link } from "./Link";
  */
 export const Tags = ({ tags }) => (
   <div className="flex flex-wrap gap-2 font-sans">
-    {tags.map((tag) => (
-      <Link href={`/blog/tags/${tag}`}>
+    {sortTags(tags).map((tag) => (
+      <Link href={`/blog/tags/${tag}`} key={tag}>
         <small className="rounded-md border border-current py-1 px-2" key={tag}>
           {`#${tag}`}
         </small>
@@ -14,3 +14,10 @@ export const Tags = ({ tags }) => (
     ))}
   </div>
 );
+
+/**
+ * @param {ReadonlyArray<string>} tags
+ */
+function sortTags(tags) {
+  return [...tags].sort((a, b) => a.localeCompare(b));
+}

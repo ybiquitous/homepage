a42b666d290285441d613afbc9fc19d074e137a3
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-17 13:23:26 +0900

    Stop using `font-serif` and customize `font-sans`

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -4,7 +4,7 @@ import { Link } from "./Link";
  * @param {{ tags: ReadonlyArray<string> }} props
  */
 export const Tags = ({ tags }) => (
-  <div className="flex flex-wrap gap-2 font-sans">
+  <div className="flex flex-wrap gap-2">
     {sortTags(tags).map((tag) => (
       <Link href={`/blog/tags/${tag}`} key={tag}>
         <small className="rounded-md border border-current py-1 px-2" key={tag}>

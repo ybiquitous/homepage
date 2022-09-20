87d332336da9c8c54bc739a0baa3b793223bcd38
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-20 23:28:03 +0900

    Remove needless comments

--- src/pages/BlogPost/replaceSpeakerdeck.js
+++ src/pages/BlogPost/replaceSpeakerdeck.js
@@ -4,10 +4,6 @@
  */
 export function replaceSpeakerdeck(el) {
   for (const deck of el.querySelectorAll(".speakerdeck-iframe")) {
-    // if (!(deck instanceof HTMLElement)) continue;
-
-    // const newDeck = deck.clone(true)
-    // debugger
     deck.parentElement?.replaceWith(deck);
   }
 }

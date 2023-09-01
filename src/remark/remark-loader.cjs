f2957cd63c8a3219a0821cc66a025bec8a2675ef
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-09-01 12:41:24 +0900

    Remove unneeded `remark-loader.cjs`
    Webpack can now read loaders written in ESM. So, bridging from CJS to ESM is no longer needed.

--- src/remark/remark-loader.cjs
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-// TODO: When Webpack supports ESM fully, this bridge module (CJS to ESM) will be no longer necessary.
-module.exports = function remarkLoader(...args) {
-  return import("./remark-loader.js").then((loader) => loader.default(...args));
-};

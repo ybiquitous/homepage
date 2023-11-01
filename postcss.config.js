c0b3327de6a04906f955993797932b4b62fea805
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-11-01 20:42:38 +0900

    Remove needless `@ts-expect-error`

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ import process from "node:process";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import tailwindcss from "tailwindcss";
-// @ts-expect-error -- TS7016: Could not find a declaration file for module 'tailwindcss/nesting/index.js'.
 import tailwindcssNesting from "tailwindcss/nesting/index.js";
 /* eslint-enable import/no-extraneous-dependencies */
 

2529a94a91f0b680093bd3a7a649d36456b6f825
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-05-20 23:59:05 +0900

    Add blog post generator
    Usage:
    
    ```shell
    node --run=blog-new
    ```

---
+++ scripts/new-blog-post.js
@@ -0,0 +1,70 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output, exit } from "node:process";
+import { styleText } from "node:util";
+
+const projectRoot = path.resolve(import.meta.dirname, "..");
+const blogRoot = path.resolve(projectRoot, "src/blog");
+
+async function main() {
+  const rl = readline.createInterface({ input, output });
+
+  try {
+    const publishedTime = new Date();
+    publishedTime.setHours(publishedTime.getHours() + 3);
+    const thisYear = publishedTime.getFullYear();
+    const thisYearDir = path.resolve(blogRoot, `${thisYear}`);
+
+    const title = await rl.question(styleText("bold", "Title? "));
+    const slug = title.trim().replaceAll(/[?!]/g, "").replaceAll(/\s+/g, "-").toLowerCase();
+    const blogFile = path.resolve(thisYearDir, `${slug}.md`);
+    const blogFileRelative = path.relative(projectRoot, blogFile);
+
+    if (fs.existsSync(blogFile)) {
+      const yesNo = await rl.question(
+        `${styleText("bold", blogFileRelative)} already exists. Overwrite? [y/N] `,
+      );
+
+      if (yesNo.toLowerCase() !== "y") {
+        console.log("Abort.");
+        return 1;
+      }
+    }
+
+    const tags = (await rl.question(styleText("bold", "Tags? ")))
+      .trim()
+      .split(/[\s,]+/)
+      .filter(Boolean);
+
+    const yesNo = await rl.question(`Write ${styleText("bold", blogFileRelative)}? [Y/n] `);
+
+    if (yesNo.toLowerCase() === "n") {
+      console.log("Abort.");
+      return;
+    }
+
+    if (!fs.existsSync(thisYearDir)) {
+      fs.mkdirSync(thisYearDir);
+    }
+
+    const blogContent = `
+---
+published: ${publishedTime.toISOString()}
+lastUpdated: null
+author: Masafumi Koba
+tags: ${tags.join(", ")}
+---
+
+# ${title}
+`.trimStart();
+
+    fs.writeFileSync(blogFile, blogContent);
+
+    console.log("Done.");
+  } finally {
+    rl.close();
+  }
+}
+
+main().then((code) => exit(code));

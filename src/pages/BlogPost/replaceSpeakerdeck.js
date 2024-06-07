76072e0b3d4098399d9d8b1b0d35d5b1370c62fc
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-06-07 16:22:06 +0900

    Remove unnecessary Speaker Deck slide margin

--- src/pages/BlogPost/replaceSpeakerdeck.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {HTMLElement} el
- * @returns {void}
- */
-export function replaceSpeakerdeck(el) {
-  for (const deck of el.querySelectorAll(".speakerdeck-iframe")) {
-    deck.parentElement?.replaceWith(deck);
-  }
-}

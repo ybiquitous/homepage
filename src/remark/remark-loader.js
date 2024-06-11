927fd599954bb0e1b510e7767943f8db39584c8e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-06-11 15:57:24 +0900

    Fix ESLint issues

--- src/remark/remark-loader.js
+++ src/remark/remark-loader.js
@@ -4,9 +4,9 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeHighlight from "rehype-highlight";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
-import { remarkAlert } from "remark-github-blockquote-alert";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkGfm from "remark-gfm";
+import { remarkAlert } from "remark-github-blockquote-alert"; // eslint-disable-line import/no-unresolved
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";

361c8e63e53a6c48a1655e4d71518ea1159ba7c5
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-01-05 17:20:26 +0900

    Change Twitter to X on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { faGithub, faNpm, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faNpm, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faBlog, faGem, faImages } from "@fortawesome/free-solid-svg-icons";
 import { Title } from "../components/Title";
 import { Menu } from "./Home/Menu";
@@ -18,7 +18,7 @@ export const Home = () => (
           <Menu link="/blog" icon={faBlog} content="Blog" />
           <Menu link="/slides" icon={faImages} content="Slides" />
           <Menu link="https://github.com/ybiquitous" icon={faGithub} content="GitHub" />
-          <Menu link="https://twitter.com/ybiquitous" icon={faTwitter} content="Twitter" />
+          <Menu link="https://x.com/ybiquitous" icon={faXTwitter} content="X" />
           <Menu link="https://www.npmjs.com/~ybiquitous" icon={faNpm} content="npm" />
           <Menu link="https://rubygems.org/profiles/ybiquitous" icon={faGem} content="RubyGems" />
         </div>

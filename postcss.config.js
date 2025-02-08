99115ba2de2d6421059fb8309b8fa1dc093b3f0e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2025-02-08 17:24:26 +0900

    Stop building CSS Nesting
    Today (2025), CSS Nesting has been accepted as Baseline 2023.
    Ref https://caniuse.com/css-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ import process from "node:process";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import tailwindcss from "tailwindcss";
-import tailwindcssNesting from "tailwindcss/nesting/index.js";
 /* eslint-enable import/no-extraneous-dependencies */
 
 const PRODUCTION = process.env["NODE_ENV"] === "production";
@@ -11,7 +10,6 @@ const PRODUCTION = process.env["NODE_ENV"] === "production";
 const config = {
   plugins: [
     // The order is important.
-    tailwindcssNesting,
     tailwindcss,
     autoprefixer,
     PRODUCTION ? cssnano : undefined,
